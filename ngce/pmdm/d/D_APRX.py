28f107920757d84306eb028aeac556d136054633
Jeffrey Scarmazzi <jeff8977@esri.com>
2017-08-09 15:44:18 -0400

    8/9

---
+++ ngce/pmdm/d/D_APRX.py
@@ -0,0 +1,70 @@
+from ngce.pmdm.d.D_Config import *
+import arcpy
+import os
+
+
+def collect_table_inputs(j_id):
+
+    print('Collecting Inputs From Database Table')
+
+    j_id = int(j_id)
+
+    table = JOB_SOURCE
+
+    values = []
+    with arcpy.da.SearchCursor(table, ['WMX_Job_ID', 'Project_ID', 'Project_Dir']) as cursor:
+        for r in cursor:
+            if r[0] == j_id:
+                values.append(r[1])
+                values.append(r[2])
+
+    if not values:
+        raise Exception('Script Was Unable to Acquire Inputs From Table. Please Check Job ID')
+    else:
+        return values
+
+
+def project_set(base_dir, project_id):
+
+    base_aprx = arcpy.mp.ArcGISProject(BASE_APRX)
+    new_aprx = os.path.join(base_dir, project_id + '.aprx')
+    base_aprx.saveACopy(new_aprx)
+    aprx = arcpy.mp.ArcGISProject(new_aprx)
+
+    return aprx
+
+
+if __name__ == '__main__':
+
+    # Collect Job ID from Command Line
+    job_id = '808'
+
+    # Collect Script Inputs from SDE Table
+    inputs = collect_table_inputs(job_id)
+    project_id, project_dir = inputs[0], inputs[1]
+
+    # Set Path For APRX
+    derived_dir = os.path.join(project_dir, DERIVED)
+    base_dir = os.path.join(derived_dir, 'Editing')
+    os.mkdir(base_dir)
+
+    # Return Map Object
+    aprx = project_set(base_dir, project_id)
+    base_map = aprx.listMaps('*')[0]
+
+    # Reference LASD
+    target_lasd = os.path.join(derived_dir, project_id + '.lasd')
+
+    # Reference D03 Output
+    d03_output = os.path.join(derived_dir, D03, 'RESULTS', 'd03_final.shp')
+
+    # Reference D01 Fishnet
+    d01_fishnet = os.path.join(derived_dir, D01, 'FISHNET', 'fishnet.shp')
+
+    # Add Inputs To Map
+    base_map.addDataFromPath(target_lasd)
+    base_map.addDataFromPath(d01_fishnet)
+    base_map.addDataFromPath(d03_output)
+
+    # Save APRX
+    aprx.save()
--- ngce/pmdm/d/D_Config.py
+++ ngce/pmdm/d/D_Config.py
@@ -10,3 +10,6 @@ D05 = 'D05'
 
 # collect_table_inputs (D01, D03, D04, D05)
 JOB_SOURCE = r'C:\Users\jeff8977\Desktop\NGCE_GitHub\CMDR.gdb\ProjectJob'
+
+# Base ArcGIS Pro Project For Mapping Scripts
+BASE_APRX = r'C:\Users\jeff8977\Desktop\NGCE_GitHub\ngce\pmdm\d\APRX\D_Base.aprx'
